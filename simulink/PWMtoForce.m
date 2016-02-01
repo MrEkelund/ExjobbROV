6e16a00fad91fd952a3ade9454e09682405dbb31
MrEkelund <erikekelund91@gmail.com>
2016-02-01 14:58:04 +0100

    Edited toPWM with description and new input format. Modified decoupler modules syntax and inputs.

--- simulink/PWMtoForce.m
+++ simulink/PWMtoForce.m
@@ -1,7 +1,9 @@
 function [force] = PWMtoForce(pwm_in)
-%Converts PWm signal in us to force in newtons
+%Converts PWM signal in us to force in newtons
 % several doifferent sets of coefficients are supplied
 % Data used for poyfit comes from BlueROV extra folder.
+
+%uncommet the desiered p value.
 % p = [-6.2137   -1.9239   15.5456    3.7720  -14.4402   -2.7320    6.6768
 % 1.2707    0.5515   -0.0028];
 p = [-1.3536   -0.4257    2.5444    0.8196    0.9102    0.0111];
