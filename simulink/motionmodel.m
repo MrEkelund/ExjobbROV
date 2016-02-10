b0bc869eef18a0811f82f3e314325c05a80fe324
Adam <a_aili@hotmail.com>
2016-02-10 15:43:25 +0100

    Added some to the model

--- simulink/motionmodel.m
+++ simulink/motionmodel.m
@@ -1,27 +1,81 @@
-function [ output_args ] = Untitled( input_args )
+function [dx, y] = rov_motion_model(t, x, control, Ix, Iy, Iz, Zw_dot,...
+        Yv_dot, Xu_dot, Mq_dot, Nr_dot,Kp_dot, varargin)
+%******* Constants
+m = ?;      % ROV mass[kg];   
+g = 9.80;   % Gravity[m/s^2]
+rho =? ;    % water density [kg/m^3]
+V = ?;      % Discplaced water volume [m^3]
+%******* Inputs
 
 
-M = diag(m, m, m, Ix, Iy, Iz)...
-    + diag(Xu_dot,Yv_dot,Zw_dot,Kp_dot,Mq_dot,Nr_dot);
+%******* States
+u = x();
+v = x();
+w = x();
+p = x();
+q = x();
+r = x();
 
 
+%******* Computed values
+ct = cos(theta);
+st = sin(theta);
+cf = cos(fi);
+sf = sin(fi);
+cp = cos(psi);
+sp = sin(psi);
 
-C_added=...
-    [0 0 0 0 -Zw_dot*w Yv_dot*v;
-    0 0 0 Zw_dot*w 0 -Xu_dot*u;
-    0 0 0 -Yv_dot*v Xu_dot*u 0;
-    0 -Zw_dot*w Yv_dot*v 0 -Nr_dot*r Mq_dot*q;
-    Zw_dot*w 0 -Xu_dot*u Nr_dot*r 0 -Kp_dot*p;
-    -Yv_dot*v Xu_dot*u 0 -Mq_dot*q Kp_dot*p 0];
+B = rho*g*V;
+W = m*g;
+
+%******* Model
+M = diag([m, m, m, Ix, Iy, Iz])...
+    + diag([Xu_dot,Yv_dot,Zw_dot,Kp_dot,Mq_dot,Nr_dot]);
 
 omega = [p, q, r]';
 vel = [u, v, w]';
-C_rigid = m*cross(omega,vel); % Ej klar
-C = C_added + C_rigid;
 
+C_A = [...
+        -Zw_dot*w*q+Yv_dot*v*r;
+        Zw_dot*w*p-Xu_dot*u*r;
+        -Yv_dot*v*p+Xu_dot*u*q;
+        (-Zw_dot+Yv_dot)*v*w+(Mq_dot-Nr_dot)*q*r;
+        (Zw_dot-Xu_dot)*u*w+(Nr_dot-Kp_dot)*p*r;
+        (-Yv_dot+Xu_dot)*u*v+(Kp_dot-Mq_dot)*p*q];
 
+% def (3.57) page 55 fossen
+C_RB = [...
+    m*(q*w-r*v);
+    m*(r*u-p*w);
+    m*(p*v-q*u);
+    q*r*(Iy-Iz);
+    r*p*(Iz-Ix);
+    q*p*(Ix-Iy)];
 
+C = C_A + C_RB;    
 
+D = -diag([...
+    (Xu + Xu_abs_u*abs(u))*u,(Yv + Yv_abs_v*abs(v))*v,...
+    (Zw + Zw_abs_w*abs(w))*w,(Kp + Kp_abs_p*abs(p))*p,...
+    (Mq + Mq_abs_q*abs(q))*q,(Nr + Nr_abs_r*abs(r))*r]);
+
+g = [...
+    (W - B)*st;
+    -(W - B)*ct*sf;
+    -(W - B)*ct*sf;
+    -zb*B*ct*sf;
+    -zb*B*st;
+    0];
 
 end
+
+
+
+
+
+
+
+
+
+
 
