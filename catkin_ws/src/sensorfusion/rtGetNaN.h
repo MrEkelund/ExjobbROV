239167e539519e0b7b1da61ffc2f8492b63414d0
MrEkelund <erikekelund91@gmail.com>
2016-03-03 13:51:47 +0100

    final version of sensor fusion. Codegen working with some minor warnings.

---
+++ catkin_ws/src/sensorfusion/rtGetNaN.h
@@ -0,0 +1,46 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: rtGetNaN.h
+//
+// Code generated for Simulink model 'sensorfusion'.
+//
+// Model version                  : 1.288
+// Simulink Coder version         : 8.9 (R2015b) 13-Aug-2015
+// C/C++ source code generated on : Thu Mar  3 10:30:28 2016
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
+// Code generation objectives:
+//    1. Execution efficiency
+//    2. RAM efficiency
+// Validation result: Not run
+//
+
+#ifndef RTW_HEADER_rtGetNaN_h_
+#define RTW_HEADER_rtGetNaN_h_
+#include <stddef.h>
+#include "rtwtypes.h"
+#include "rt_nonfinite.h"
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+  extern real_T rtGetNaN(void);
+  extern real32_T rtGetNaNF(void);
+
+#ifdef __cplusplus
+
+}                                      // extern "C"
+#endif
+#endif                                 // RTW_HEADER_rtGetNaN_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
