89c525ddab7ef23c0097bfb4dea8a4a9f93851ed
Adam <a_aili@hotmail.com>
2016-03-14 11:12:34 +0100

    sime

--- simulink/Estimation/fossentest.m
+++ simulink/Estimation/fossentest.m
@@ -1,12 +1,16 @@
- r= 1000; 
- acc_matrix = repmat([6.9343 0 -6.9343],[r 1]);
+ endtime = 40;
+ r= 100 * endtime; 
+ acc_matrix = repmat([0 0 9.806],[r 1]);
  yaw = repmat([0], [r ,1]);
  roll =repmat([0], [r ,1]);
- pitch = repmat([45], [r ,1]);
+ pitch = repmat([0], [r ,1]);
  depth = repmat([0], [r ,1]);
- k1 = [1 1 1];
- k2 = [1 1 1];
- k3 = [0.1 0.1 0.1]*1;
- time = linspace(0,10,r)';
+ k1 = diag([0.01 0.01 0.1]);
+ k2 = diag([1 1 1]);
+ k3 = diag([0.1 0.1 0.1]*1);
+ time = linspace(0,endtime,r)';
  [a b] = fossenIntegration(acc_matrix, roll , pitch, yaw,depth,k1, k2, k3, time);
- a
+ a;
+ 
+ plot(time(1:end-1), a)
+ legend('x','y','z')
