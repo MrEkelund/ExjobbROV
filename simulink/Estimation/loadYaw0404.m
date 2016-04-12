7edd16ee2febfcfee89f964b63b70a6ddddaf810
Adam <a_aili@hotmail.com>
2016-04-12 16:17:39 +0200

    Parameters

--- simulink/Estimation/loadYaw0404.m
+++ simulink/Estimation/loadYaw0404.m
@@ -5,6 +5,9 @@ yaw_filepath = fullfile('bag','test1_t3_t4_2016-04-04-14-57-52.bag');
 [lin_vel_data ,lin_acc_data, ang_vel_data, thrusters_data, states, time, ~]= ...
 getTestData(yaw_filepath, 0);
 output_data = [ang_vel_data, antiModAngles(states(:,1:3))];
+out_mean = mean(output_data);
+out_mean(:,3:end) = 0;
+output_data = output_data - repmat(out_mean, size(output_data,1),1);
 input_data = thrusters_data;
 yaw_data1 = iddata(output_data, input_data, Ts);
 %%
@@ -12,6 +15,9 @@ yaw_filepath = fullfile('bag','test2_t3_t4_2016-04-04-14-59-57.bag');
 [lin_vel_data ,lin_acc_data, ang_vel_data, thrusters_data, states, time, ~]= ...
 getTestData(yaw_filepath, 0);
 output_data = [ang_vel_data, antiModAngles(states(:,1:3))];
+out_mean = mean(output_data);
+out_mean(:,3:end) = 0;
+output_data = output_data - repmat(out_mean, size(output_data,1),1);
 input_data = thrusters_data;
 yaw_data2 = iddata(output_data, input_data, Ts);
 %%
@@ -19,6 +25,9 @@ yaw_filepath = fullfile('bag','test3_t3_t4_2016-04-04-15-02-24.bag');
 [lin_vel_data ,lin_acc_data, ang_vel_data, thrusters_data, states, time, ~]= ...
 getTestData(yaw_filepath, 0);
 output_data = [ang_vel_data, antiModAngles(states(:,1:3))];
+out_mean = mean(output_data);
+out_mean(:,3:end) = 0;
+output_data = output_data - repmat(out_mean, size(output_data,1),1);
 input_data = thrusters_data;
 yaw_data3 = iddata(output_data, input_data, Ts);
 %%
