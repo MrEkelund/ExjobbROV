678d151279054f2cdbb18d4e662c2e0711c1e84b
MrEkelund <erikekelund91@gmail.com>
2016-04-22 17:04:38 +0200

    new script files

--- simulink/Estimation_raw/kalmanEstimator.m
+++ simulink/Estimation_raw/kalmanEstimator.m
@@ -1,12 +1,13 @@
 %% load data set
 Ts = 1/100;
+fs= 1/Ts;
 data = loadAll0418(0,1/Ts);
 %%
 for k=1:length(data.exp)
     exp = getexp(data,k);
     tau = thrusterLookup(exp.InputData);
-    %%
     measurements = exp.OutputData';
+    measurements(1:3,:)=detrend(measurements(1:3,:),'constant');
     g = 9.81744;
     %set the first magnetometer reading
     mag_n = measurements(7,1);
@@ -17,8 +18,8 @@ for k=1:length(data.exp)
     
     % Set initial variables
     P = blkdiag(1000*eye(7),1*eye(16));
-    Q = blkdiag(1000*eye(3),0.01*eye(16));
-    R = blkdiag(0.01*eye(3),0.1*eye(3),eye(3));
+    Q = blkdiag(1000*eye(3),0.1*eye(16));
+    R = blkdiag(0.001*eye(3),0.001*eye(3),eye(3));
     state = zeros(23,2*length(measurements));
     
     % First run variables
@@ -45,11 +46,10 @@ for k=1:length(data.exp)
         state(8:end,1) = [zb; Kp; Kp_dot; Kp_abs_p; Mq;...
             Mq_dot; Mq_abs_q; Nr; Nr_dot; Nr_abs_r;...
             Ix; Iy; Iz; Ix_Kp_dot; Iy_Mq_dot; Iz_Nr_dot];
-        display('first')
     else
-        state(8:end,1)=PreviousEstimate;
-        display('Not first')
+        state(8:end,1) = state_last_it;  
     end
+    display(sprintf('Data set number %i',k))
     
     %run Kalman
     j = 1;
@@ -68,7 +68,42 @@ for k=1:length(data.exp)
         P = F*P*F.' + Gv*Q*Gv.';
         j=j+1;
     end
-    % save
-    PreviousEstimate = state(8:end,end)
+    state_last_it = state(8:end,end);
+    params(:,k) = state_last_it;
 end
+EstimatedParams=mean(params');
+%% compare against validation data
+%chose which dataset to validate with.
+dataset = 1;
+valid = getexp(data,dataset);
+tau_valid = thrusterLookup(valid.InputData);
+y_valid = valid.outputdata';
+initialCondition = [1;0;0;0;y_valid(1:3,1)];
+t=(0:1/fs:(length(tau_valid)-1)/fs);
+tau_valid = [t;tau_valid];
+save('tau.mat','tau_valid');
+save('config.mat','EstimatedParams','initialCondition');
+sim('quatSim',t(end));
+clf;
+subplot(3,1,1)
+plot(t,[pqr.data(:,1),y_valid(1,:)'])
+str = sprintf('p: Fit %f %%',100*goodnessOfFit(pqr.data(:,1),y_valid(1,:)','NMSE'));
+title(str)
+subplot(3,1,2)
+plot(t,[pqr.data(:,2),y_valid(2,:)'])
+str = sprintf('q: Fit %f %%',100*goodnessOfFit(pqr.data(:,2),y_valid(2,:)','NMSE'));
+title(str)
+subplot(3,1,3)
+plot(t,[pqr.data(:,3),y_valid(3,:)'])
+str = sprintf('r: Fit %f %%',100*goodnessOfFit(pqr.data(:,3),y_valid(3,:)','NMSE'));
+title(str)
+
+
+
+
+
+
+
+
+
 
