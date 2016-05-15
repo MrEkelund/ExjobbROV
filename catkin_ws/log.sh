524daffbb8ea0558e622c5c177847f6df94153e4
Adam <a_aili@hotmail.com>
2016-05-15 17:34:45 +0200

    Added ref to log

--- catkin_ws/log.sh
+++ catkin_ws/log.sh
@@ -2,7 +2,7 @@
 #export ROS_MASTER_URI=http://bluerov:11311
 #export ROS_IP=$(gethostip -d workstation)
 
-echo -e "Enter the name you want for the logfile" 
+echo -e "Enter the name you want for the logfile"
 read name
 cd ../simulink/Estimation/bag
-rosbag record -o $name sensor_fusion/states rovio/water_pressure/data rovio/magnetometer/data rovio/imu/data rovio/thrusters
+rosbag record -o $name sensor_fusion/states rovio/water_pressure/data rovio/magnetometer/data rovio/imu/data rovio/thrusters rovio/ref
